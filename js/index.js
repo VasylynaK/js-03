11ad55a3d54e808961e3276b077bf483c2e79c0b
Vasylyna Katsma <v.katsma@gmail.com>
2017-04-04 20:26:20 +0000

    Fibonacci Function

---
+++ js/index.js
@@ -0,0 +1,23 @@
+var n = Number(prompt('please input number n'));
+
+document.write('n-ne fibRec = ' + fibRec(n));
+
+function fibRec(n) {
+    if (n < 3) {
+        return 1;
+    }
+    return fibRec(n - 1) + fibRec(n - 2);
+}
+
+function fibLoop(n) {
+    var a = 1;
+    var b = 1;
+
+    for (var i = 3; i <= n; i++) {
+        a = a + b;
+        b = a - b;
+    }
+
+    return a;
+}
+document.write('<br>' + 'n-ne fibLoop = ' + fibLoop(n));
